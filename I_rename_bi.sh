37a9af0a47fb9f7faa049538381535582193d5c8
salvatoreloguercio <salvatore.loguercio@gmail.com>
2023-03-23 19:37:58 -0700

    I_rename_bi.sbatch - more fixes

--- I_rename_bi.sh
+++ I_rename_bi.sh
@@ -10,7 +10,7 @@ while read path_line; do
 
  vcf=$(basename $path_line .vcf.gz | cut -d "_" -f 1-2 )
 
-  sbatch --export=input=$path_line --job-name=I_rename_bi_$vcf --output=I_rename_bi_$vcf.out I_rename_bi.sbatch;
+  sbatch --export=input=$path_line --job-name=I_rename_bi_$vcf --output=I_rename_bi_$vcf.%J I_rename_bi.sbatch;
   sleep 1;
 
 done < test.txt
