2c6a0e39217b2407bd303516fe624e972d1e296d
salvatoreloguercio <salvatore.loguercio@gmail.com>
2023-03-24 19:20:51 -0700

    I_rename_bi.sbatch - fixed issue with same CG naming

--- I_rename_bi.sh
+++ I_rename_bi.sh
@@ -8,9 +8,9 @@ done
 
 while read path_line; do
 
- vcf=$(basename $path_line .vcf.gz | cut -d "_" -f 1-2 )
+ vcf=$(basename $path_line .vcf.gz | sed -e "s/_TOPMed_WGS_freeze\.8\.chr22\.hg38//g") #cut -d "_" -f 1-2 )
 
   sbatch --export=input=$path_line --job-name=I_rename_bi_$vcf --output=I_rename_bi_$vcf.%J I_rename_bi.sbatch;
   sleep 1;
 
-done < test.txt
+done < TOPmed_GRch38_frz8_paths.tsv
