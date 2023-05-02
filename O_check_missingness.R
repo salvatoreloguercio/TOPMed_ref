f3b9b83fb7ea4475fcced4527871d40a1c7259df
salvatoreloguercio <salvatore.loguercio@gmail.com>
2023-05-01 17:56:09 -0700

    R summarizer script for missingness

---
+++ O_check_missingness.R
@@ -0,0 +1,13 @@
+
+#! /usr/bin/Rscript
+
+options(stringsAsFactors = F)
+
+
+filelist = list.files(pattern="*.smiss$")
+
+smiss_summaries<-do.call("rbind",lapply(filelist,function(x) summary(read.delim(x,header=T)$MISSING_CT)))
+
+rownames(smiss_summaries)<-gsub(".smiss","",filelist)
+
+write.table(smiss_summaries,sep="\t",row.names=T,sep="",quote=F,file="O_missingness_summaries.tsv")
